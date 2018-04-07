2a45358622e262b6f45758f66500276ab8e39916
itcast <bos@itcast.cn>
2018-04-07 20:09:44 +0800

    init first commit

---
+++ dubboxdemo_service/src/main/java/cn/itcast/dubboxdemo/service/impl/UserviceImpl.java
@@ -0,0 +1,12 @@
+package cn.itcast.dubboxdemo.service.impl;
+
+import cn.itcast.dubboxdemo.service.UserService;
+import com.alibaba.dubbo.config.annotation.Service;
+
+@Service
+public class UserviceImpl implements UserService {
+    @Override
+    public String getName() {
+        return "itheima";
+    }
+}
---
+++ dubboxdemo_web/src/main/java/cn/itcast/demo/controller/UserController.java
@@ -0,0 +1,21 @@
+package cn.itcast.demo.controller;
+
+import cn.itcast.dubboxdemo.service.UserService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Reference
+    private UserService userService;
+
+    @RequestMapping("/showName")
+    @ResponseBody
+    public String showName(){
+        return userService.getName();
+    }
+}
